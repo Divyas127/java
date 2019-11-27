7d31558c01914458624a646cde22391635b12f4f
Divyas127 <58220171+Divyas127@users.noreply.github.com>
2019-11-27 19:16:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("This is github practice from eclsipse- Version 1");
+      System.out.println("This is github practice from Github- Version 1");
    }
 }
